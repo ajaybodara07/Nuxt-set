09503addf5ac7f607539519ce2873faa512e1f0e
ajay bodara <ad.bodara@gmail.com>
2022-05-20 23:40:21 +0530

    add changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,10 @@
   <div>
     <nuxt/>
     <footer class="info">
+      <p>Todo Nuxt Set</p>
 			<p>Double-click to edit a todo</p>
 			<p>Written with <a href="https://nuxtjs.org">Nuxt.js</a></p>
-			<p><a href="http://todomvc.com">TodoMVC</a></p>
+			<p><a href="http://todomvc.com">Todo MVC</a></p>
 		</footer>
   </div>
 </template>
